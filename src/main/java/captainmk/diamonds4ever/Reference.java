3e7ede234b0ef9faf4e9d2bf6d1abb4713c59a30
captainmk <captainmk@cs.nmt.edu>
2019-09-12 14:56:09 -0400

    Initial commit

---
+++ src/main/java/captainmk/diamonds4ever/Reference.java
@@ -0,0 +1,8 @@
+package captainmk.diamonds4ever;
+
+public class Reference {
+    public static final String MODID = "cmk_daf";
+    public static final String MODNAME = "Diamonds are Forever";
+    public static final String VERSION = "0.0.1";
+    public static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12]";
+}
